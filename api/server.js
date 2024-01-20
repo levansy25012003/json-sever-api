d502b659ee8213d9d49a20f0a09436f3625280ff
levansy25012003 <levansy25012003@gmail.com>
2024-01-20 11:01:56 +0700

    update json-server

--- api/server.js
+++ api/server.js
@@ -1,24 +1,24 @@
-// JSON Server module
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const jsonServer = require('json-server')
+const server = jsonServer.create()
 
-// Make sure to use the default middleware
-const middlewares = jsonServer.defaults();
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
-server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
-server.use(router);
-// Listen to port
-server.listen(3000, () => {
- console.log("JSON Server is running");
-});
+const middlewares = jsonServer.defaults()
 
-// Export the Server API
-module.exports = server;
+server.use(middlewares)
+
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(8080, () => {
+    console.log('JSON Server is running')
+})
+
+module.exports = server
